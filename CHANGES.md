42eb6b85142f05a54bbfc596b93ffb420187f0e2
Jeff Dairiki <dairiki@dairiki.org>
2022-10-07 18:30:14 -0700

    Use explicit encoding in custom hatch metadata hook
    This fixes build-time exception on Windows.

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,11 @@
 ## Changes
 
+### 0.1.0b2 (unreleased)
+
+#### Bugs
+
+- Use explicit encoding in hatch metadata hook (for Windows).
+
 ### 0.1.0b1 (2022-10-07)
 
 Initial release.
--- hatch_build.py
+++ hatch_build.py
@@ -7,8 +7,8 @@ from hatchling.metadata.plugin.interface import MetadataHookInterface
 
 class ReadmeMetadataHook(MetadataHookInterface):
     def update(self, metadata: Dict[str, Any]) -> None:
-        readme = Path(self.root, "README.md").read_text().strip()
-        changes = Path(self.root, "CHANGES.md").read_text().strip()
+        readme = Path(self.root, "README.md").read_text("utf-8").strip()
+        changes = Path(self.root, "CHANGES.md").read_text("utf-8").strip()
 
         metadata["readme"] = {
             "content-type": "text/markdown",
